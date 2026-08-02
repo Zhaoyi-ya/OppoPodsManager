ce6abe4f7a1629fb211e034dd4351097abc984ab
dszsu <dsz1234567809@outlook.com>
2026-08-03 00:34:24 +0800

    撤回渲染器更换

--- App/Program.cs
+++ App/Program.cs
@@ -1,8 +1,4 @@
-using System;
 using Avalonia;
-#if WINDOWS
-using Avalonia.Win32;
-#endif
 
 namespace OppoPodsManager;
 
@@ -10,28 +6,9 @@ static class Program
 {
     // Avalonia configuration, don't remove; used by Previewer.
     public static AppBuilder BuildAvaloniaApp()
-    {
-        var builder = AppBuilder.Configure<App>()
-            .UsePlatformDetect();
-
-#if WINDOWS
-        var useSoftwareRendering = !string.Equals(
-            Environment.GetEnvironmentVariable("OPPO_PODS_GPU_RENDERING"), "1", StringComparison.Ordinal)
-            || string.Equals(Environment.GetEnvironmentVariable("OPPO_PODS_SOFTWARE_RENDERING"), "1", StringComparison.Ordinal);
-        if (useSoftwareRendering)
-        {
-            var options = new Win32PlatformOptions
-            {
-                RenderingMode = [Avalonia.Win32RenderingMode.Software],
-            };
-            if (string.Equals(Environment.GetEnvironmentVariable("OPPO_PODS_REDIRECTION_COMPOSITION"), "1", StringComparison.Ordinal))
-                options.CompositionMode = [Avalonia.Win32CompositionMode.RedirectionSurface];
-            builder = builder.With(options);
-        }
-#endif
-
-        return builder.LogToTrace();
-    }
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
 
     /// <summary>Avalonia 桌面入口点（AOT 兼容）。</summary>
     public static void Main(string[] args)
